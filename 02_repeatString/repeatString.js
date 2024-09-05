52997cf7ff1d35e7b43c8c554b01411ee0d199fe
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-05 17:55:10 -0300

    Finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return 'ERROR'
+    }
+    else {
+    let rep = string.repeat(times)
+    return rep
+    }
 };
 
 // Do not edit below this line
