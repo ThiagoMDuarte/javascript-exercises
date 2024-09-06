015e444439e74791a2aecb4efb23812cede55a4d
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-06 19:08:10 -0300

    Finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string){
+    let tamanho = string.length * -1
+    let newString = ''
+    for (let i = -1; i >= tamanho  ; i--){
+      console.log(newString += string.at(i))
+    }
+    return newString
 };
 
 // Do not edit below this line
