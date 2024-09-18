7f2c0949dc587e1b04d1c8d3ae1b3a402f68b3c5
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-17 22:22:49 -0300

    FInish exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0
+  } else if (array.length == 1){
+    return array[0]
+  } return array.reduce((total,num) => {
+    return total + num
+  },0)
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce((total,num) => {
+    return total * num
+  },1)
 
 };
-
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  var f = []
+	if (num == 1 || num == 0) {
+    return 1
+  } if (f[num] > 0) {
+    return f[num] }
+  return f[num] = factorial(num-1) * num
 };
 
 // Do not edit below this line
