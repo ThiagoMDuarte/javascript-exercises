e91e170ce08fc5ecc2b4b8c1e14f93b00dbfb132
Thiago Duarte <104696554+ThiagoMDuarte@users.noreply.github.com>
2024-09-18 12:24:21 -0300

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fib = []
+    if (num < 0){
+        return 'OOPS'
+    }
+    if (num == 0) {
+        return 0
+    }
+    if (num == 1 || num == 2) {
+        return 1
+    }
+    if (fib[num] > 0) {
+        return fib[num]
+    } return fib[num] = fibonacci(num - 1) + fibonacci(num - 2)
 };
 
 // Do not edit below this line
